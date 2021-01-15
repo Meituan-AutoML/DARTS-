adb81a8313b17d1a87593a53b0e802fc4199e9b1
Bo Zhang <zhangbo97@meituan.com>
2021-01-15 13:27:55 +0800

    chore: remove unnecessary files

--- scripts/run_rnn_search.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-gpu=0
-for i in $(seq 0 1);do
-    let gpu=$gpu+$i
-    echo $i $gpu
-    python src/rnn/train_search.py --data /home/work/dataset/penn --gpu $gpu --seed $i >& darts_train_search_ptb_seed_${i}.log &
-    sleep 1
-done
